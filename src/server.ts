ac6625ba60c11712e0af83ff4c001ad68e4a4842
thayron <thayronf3@gmail.com>
2021-03-16 20:56:29 -0300

    Foi configurado o debug

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ import { createCourse } from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({"ok":"hello word"})
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.json({"ok":"hello word"})
+})
+
+app.post('/', (request, response) => {
+  const { name } = request.body
+  response.json({"ok":"hello word"})
 })
 
 app.listen(3333, () => { console.log("servidor rodando na porta 3333") });
