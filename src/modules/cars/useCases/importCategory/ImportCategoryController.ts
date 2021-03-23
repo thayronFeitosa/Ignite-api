227bdb32fd97a66540771ecdddc68281532fc53b
thayron <thayronf3@gmail.com>
2021-03-23 09:24:25 -0300

    Foi criado a funcionalidade de upload de um arquivo csv para fazer a inserssao no array de elementos

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import path from 'path';
+import fs from 'fs';
+
+import { ImportCategoryUseCase } from './ImportCategoryUseCase';
+
+class ImportCategoryController {
+
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) {
+
+  }
+
+  handle(request: Request, response: Response) {
+    this.importCategoryUseCase.execute(request.file)
+    return response.send();
+  }
+}
+
+export { ImportCategoryController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 
 const categoriesRoute = Router();
+
+const upload = multer({
+  dest: './tmp'
+});
 
 categoriesRoute.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,6 +19,11 @@ categoriesRoute.post('/', (request, response) => {
 
 categoriesRoute.get('/', (request, response) => {
   return listCategoryController.handle(request, response);
+});
+
+categoriesRoute.post('/import', upload.single('file'), (request, response) => {
+  importCategoryController.handle(request, response);
+  response.send();
 });
 
 export { categoriesRoute };
